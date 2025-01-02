08461272cc2541b64b9ed6fa2995370183b825ef
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2025-01-02 22:48:54 +0700

    Portfolio

--- src/components/common/Card/CardProjects.vue
+++ src/components/common/Card/CardProjects.vue
@@ -2,7 +2,7 @@
   <div
     class="max-w-md border rounded-lg cursor-pointer hover:scale-105 transition-all duration-300 overflow-hidden shadow-md bg-white"
   >
-    <img :src="image" class="w-full h-40 object-cover" alt="Project Thumbnail" />
+    <img :src="image" class="w-full h-40 object-cover object-top" alt="Project Thumbnail" />
     <div class="p-4">
       <h1 class="text-lg font-semibold text-gray-800">{{ title }}</h1>
       <p class="text-xs mt-1 text-gray-600">{{ technologies.join('  ') }}</p>
