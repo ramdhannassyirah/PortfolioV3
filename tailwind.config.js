83671b0d72dad53172d1efe3766c3aa7a855d6aa
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2025-11-02 20:52:26 +0700

    Add New Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ import typography from '@tailwindcss/typography'
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [typography],
 }
