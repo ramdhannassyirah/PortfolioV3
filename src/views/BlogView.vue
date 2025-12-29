b0cf931925326a19695295285a617412cacd8fbe
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2025-12-29 23:35:25 +0700

    Fix

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -6,7 +6,7 @@
     <div class="grid gap-4 md:grid-cols-2">
       <!-- SKELETON -->
       <template v-if="loading">
-        <div v-for="n in 4" :key="n" class="animate-pulse rounded-xl border p-4">
+        <div v-for="n in 4" :key="n" class="animate-pulse rounded-xl p-4">
           <div class="h-40 w-full rounded-lg bg-gray-200"></div>
           <div class="mt-4 h-4 w-3/4 rounded bg-gray-200"></div>
           <div class="mt-2 h-4 w-1/2 rounded bg-gray-200"></div>
