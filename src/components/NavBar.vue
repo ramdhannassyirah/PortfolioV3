b935fb73a89b8ac4d83551c9044c8a50c6814246
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2026-03-11 17:34:25 +0700

    feat: implement BubbleMenu component for enhanced navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,47 @@
 <template>
-  <div>
-
-  </div>
+  <BubbleMenu :logo="''" :items="items" menu-aria-label="Toggle navigation" menu-bg="#ffffff"
+    menu-content-color="#111111" :use-fixed-position="true" animation-ease="back.out(1.5)" :animation-duration="0.5"
+    :stagger-delay="0.12" />
 </template>
 
-<script lang="ts" setup>
+<script setup>
+import BubbleMenu from '@/components/common/BubbleMenu.vue'
 
+const items = [
+  {
+    label: 'home',
+    href: '/',
+    ariaLabel: 'Home',
+    rotation: -8,
+    hoverStyles: { bgColor: '#ef4444', textColor: '#ffffff' }
+  },
+  {
+    label: 'about',
+    href: '/about',
+    ariaLabel: 'About',
+    rotation: 8,
+    hoverStyles: { bgColor: '#ef4444', textColor: '#ffffff' }
+  },
+  {
+    label: 'projects',
+    href: '#',
+    ariaLabel: 'Projects',
+    rotation: 8,
+    hoverStyles: { bgColor: '#ef4444', textColor: '#ffffff' }
+  },
+  {
+    label: 'blog',
+    href: '#',
+    ariaLabel: 'Blog',
+    rotation: 8,
+    hoverStyles: { bgColor: '#ef4444', textColor: '#ffffff' }
+  },
+  {
+    label: 'contact',
+    href: '#',
+    ariaLabel: 'Contact',
+    rotation: -8,
+    hoverStyles: { bgColor: '#ef4444', textColor: '#ffffff' }
+  }
+]
 </script>
-
-<style></style>
