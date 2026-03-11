f136348b04b951c3744c36c028b1099dae88dc9e
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2026-03-11 20:11:32 +0700

    fix: update class references for subtitle, title, and desc in Hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,20 +6,22 @@
     <div class="absolute inset-0 bg-black/40"></div>
     <!-- Content -->
     <div class="relative z-10 h-full flex items-center">
-      <div class="max-w-4xl mx-auto px-6 text-white text-center ">
-        <p class="mb-4 text-sm tracking-[0.3em] text-red-400 font-semibold uppercase">
+      <div ref="hero" class="max-w-4xl mx-auto px-6 text-white text-center">
+
+        <p class="subtitle mb-4 text-sm tracking-[0.3em] text-red-400 font-semibold uppercase">
           Full Stack Developer
         </p>
 
-        <h1 class="text-4xl md:text-7xl font-semibold leading-tight mb-6 font-noto-serif">
+        <h1 class="title text-4xl md:text-7xl font-semibold leading-tight mb-6 font-noto-serif">
           Turning Ideas Into <br>
           Scalable Web Applications
         </h1>
 
-        <p class="text-lg md:text-xl text-gray-200 mb-10 max-w-lg mx-auto">
+        <p class="desc text-lg md:text-xl text-gray-200 mb-10 max-w-lg mx-auto">
           I build fast, secure, and scalable web applications with modern technologies
           to create seamless digital experiences.
         </p>
+
       </div>
     </div>
 
@@ -33,4 +35,29 @@
 
 <script setup lang="ts">
 import CurvedLoop from './common/CurvedLoop.vue';
+import { onMounted } from 'vue';
+import gsap from 'gsap';
+
+onMounted(() => {
+
+  const tl = gsap.timeline()
+
+  tl.from(".subtitle", {
+    y: 40,
+    opacity: 0,
+    duration: 0.8
+  })
+    .from(".title", {
+      y: 60,
+      opacity: 0,
+      scale: 0.9,
+      duration: 1
+    })
+    .from(".desc", {
+      y: 40,
+      opacity: 0,
+      duration: 0.8
+    })
+
+})
 </script>
