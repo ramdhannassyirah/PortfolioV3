568711ff53c70eadf107130c485683f82dea2c3f
ramdhannassyirahh <ramdhannassyirahh@gmail.com>
2026-03-14 10:10:10 +0700

    chore: update dependencies and add legacy support for Vite
    - Updated @vitejs/plugin-vue to version 5.2.4 and added @vitejs/plugin-legacy at version 5.4.3 in package.json.
    - Changed Vite version to 5.4.21.
    - Modified vite.config.js to include legacy plugin with specified targets and additional polyfills.

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    legacy({
+      targets: ['defaults', 'not IE 11', 'ios >= 12'],
+      additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
